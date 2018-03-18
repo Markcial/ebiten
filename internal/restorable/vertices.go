7ce3b35c7def8cf09520b0779a2f3b1d7d66042e
Hajime Hoshi <hajimehoshi@gmail.com>
2018-03-18 19:51:13 +0900

    restorable: Performance optimization

--- internal/restorable/vertices.go
+++ internal/restorable/vertices.go
@@ -72,7 +72,6 @@ func (i *Image) vertices(sx0, sy0, sx1, sy1 int, geo *affine.GeoM, color *affine
 	u0, v0, u1, v1 := float32(sx0)/wf, float32(sy0)/hf, float32(sx1)/wf, float32(sy1)/hf
 
 	x, y := geo.Apply32(x0, y0)
-	cBody, cTranslate := color.UnsafeElements()
 	// Vertex coordinates
 	vs[0] = x
 	vs[1] = y
@@ -84,12 +83,6 @@ func (i *Image) vertices(sx0, sy0, sx1, sy1 int, geo *affine.GeoM, color *affine
 	vs[3] = v0
 	vs[4] = u1
 	vs[5] = v1
-	for i, v := range cBody {
-		vs[6+i] = v
-	}
-	for i, v := range cTranslate {
-		vs[22+i] = v
-	}
 
 	// and the same for the other three coordinates
 	x, y = geo.Apply32(x1, y0)
@@ -99,12 +92,6 @@ func (i *Image) vertices(sx0, sy0, sx1, sy1 int, geo *affine.GeoM, color *affine
 	vs[29] = v0
 	vs[30] = u0
 	vs[31] = v1
-	for i, v := range cBody {
-		vs[32+i] = v
-	}
-	for i, v := range cTranslate {
-		vs[48+i] = v
-	}
 
 	x, y = geo.Apply32(x0, y1)
 	vs[52] = x
@@ -113,12 +100,6 @@ func (i *Image) vertices(sx0, sy0, sx1, sy1 int, geo *affine.GeoM, color *affine
 	vs[55] = v1
 	vs[56] = u1
 	vs[57] = v0
-	for i, v := range cBody {
-		vs[58+i] = v
-	}
-	for i, v := range cTranslate {
-		vs[74+i] = v
-	}
 
 	x, y = geo.Apply32(x1, y1)
 	vs[78] = x
@@ -127,10 +108,18 @@ func (i *Image) vertices(sx0, sy0, sx1, sy1 int, geo *affine.GeoM, color *affine
 	vs[81] = v1
 	vs[82] = u0
 	vs[83] = v0
+
+	cBody, cTranslate := color.UnsafeElements()
 	for i, v := range cBody {
+		vs[6+i] = v
+		vs[32+i] = v
+		vs[58+i] = v
 		vs[84+i] = v
 	}
 	for i, v := range cTranslate {
+		vs[22+i] = v
+		vs[48+i] = v
+		vs[74+i] = v
 		vs[100+i] = v
 	}
 
